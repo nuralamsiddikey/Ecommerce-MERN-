deeb459628ddc2ea4cb569cc107cdf12018612d1
Nur Alam <nurcse253@gmail.com>
2022-09-25 20:15:07 +0600

    latest update

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,29 +1,35 @@
 import React from 'react';
 import styles from '../styles/login.module.css'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <div className={styles.wrapper}>
-              <h1 style={{textAlign:'center'}}>Welcome to Zero please login</h1>
+              <h1 style={{textAlign:'center',fontSize:'3rem'}}>Welcome to Zero please login</h1>
             <form className={styles.form}>
 
-        
              
-                   <label style={{width:'70%',marginLeft:'15%'}}>Email</label>
-                    <div class="ui input" style={{width:'70%',marginLeft:'15%'}}>   
+                   <label >Email</label>
+                    <div class="ui input" >   
                       <input type="text" placeholder="Search..."/>
                     </div>
             
                     <br />
 
-                    <label style={{width:'70%',marginLeft:'15%'}}>Password</label>
-                    <div class="ui input" style={{width:'70%',marginLeft:'15%'}}>
+                    <label >Password</label>
+                    <div class="ui input" >
                       <input type="text" placeholder="Search..."/>
-                    </div>
-       
-            
- 
-            </form>
+                    </div> 
+
+                    <p style={{marginTop:'1rem',cursor:'pointer'}}>Forgot password?</p>
+
+                    <button class="positive big ui button" style={{marginTop:'3rem'}}>
+                          Login
+                        </button>     
+
+                  <Link to="/signup" style={{marginTop:'2rem',marginLeft:'auto'}}>Don't have account?</Link>     
+                  </form>
+
         </div>
     );
 };
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,10 +1,66 @@
-import React from 'react';
+import React,{useState} from 'react';
+import { Link } from 'react-router-dom';
+import styles from '../styles/signup.module.css'
 
 const Signup = () => {
+    const [name , setName] = useState('')
+    const [email , setEmail] = useState('')
+    const [password , setPassword] = useState('')
+    
+const handleInput = ()=>{
+       if(name === ''|| email ==='' || password === ''){
+          alert('required all fields')
+       }
+}
+
+
     return (
-        <div>
-            <h1>this is signup page</h1>
-        </div>
+        <div className={styles.wrapper}>
+        <h1 style={{textAlign:'center',fontSize:'3rem'}}>Welcome to Zero please Register!</h1>
+           <form className={styles.form}>
+
+
+           <label >Full Name</label>
+              <div class="ui input" >   
+                <input 
+                    type="text"
+                    placeholder="Name"
+                    onChange={(e)=>setName(e.target.value)}
+                />
+              </div>
+       <br />
+
+             <label >Email</label>
+              <div class="ui input" >   
+                <input
+                    type="email"
+                    placeholder="Email"
+                    onChange={(e)=>setEmail(e.target.value)}
+                 />
+              </div>
+      <br />
+
+              <label >Password</label>
+              <div class="ui input" >
+                <input 
+                    type="password"
+                    placeholder="password"
+                    onChange={(e)=>setPassword(e.target.value)}
+                 />
+              </div> 
+
+
+              <button 
+                  class="positive big ui button"
+                  style={{marginTop:'3rem'}}
+                  onClick={handleInput}
+                  >
+                 Register
+             </button>     
+
+            </form>
+
+  </div>
     );
 };
 
